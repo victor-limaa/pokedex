194a89fe2a0e40bdff1af460e889d945597970c6
Victor Lima <victor-limaa@hotmail.com>
2024-09-09 22:51:53 -0300

    feat: add axios config

---
+++ src/services/apiClient.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'https://pokeapi.co/api/v2',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default apiClient;
